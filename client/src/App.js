1c970c173469507b9d62662dd0e091ffecd8a0e8
Xandi <santosal2018@gmail.com>
2023-08-09 23:22:15 -0400

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,36 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// Import Apollo tools
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink, } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// Endpoint for GraphQL
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// JWT Authorization Header Middleware For Every Request
+const authLink = setContext((_, { headers }) => {
+  // Get token from local storage if available
+  const token = localStorage.getItem('id_token');
+  // Return headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+   <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +44,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
